0b68e41c23bca9ee5ec1c07a3affc4bf26e6d677
DeveloperYamin <developer.yamin@gmail.com>
2022-04-15 14:43:04 +0600

    testing21

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,7 +20,7 @@ export default function Home({ trendingResults, followResults, providers }) {
      return (
           <Box component="section" >
                <Head>
-                    <title>Twitter Clone</title>
+                    <title>Twitter Clone NextJS</title>
                     <link rel="icon" href="/favicon.ico" />
                </Head>
 
